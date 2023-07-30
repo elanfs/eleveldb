42fab4407da3597e67de5b91b0b465b6c77e2cf7
Eric Lan <tritium86@gmail.com>
2023-07-30 22:23:58 +0800

    Add snappy and leveldb to repo

---
+++ c_src/leveldb/port/port.h
@@ -0,0 +1,15 @@
+// Copyright (c) 2011 The LevelDB Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file. See the AUTHORS file for names of contributors.
+
+#ifndef STORAGE_LEVELDB_PORT_PORT_H_
+#define STORAGE_LEVELDB_PORT_PORT_H_
+
+#include <string.h>
+#include "leveldb/ldb_config.h"
+
+// there used to be more port_*.h, but everything seems to have become
+// posix-compliant by now
+#include "port/port_posix.h"
+
+#endif  // STORAGE_LEVELDB_PORT_PORT_H_
